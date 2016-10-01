226f48e5db2ad8f23a9220c5b6fc50b46b22b89e
szymonsobczak <szymon.sobczak@getbase.com>
2016-10-01 14:17:36 -0700

    use plugin_dir_url

--- coralscroll.php
+++ coralscroll.php
@@ -10,8 +10,8 @@
  */
 
     function coralscroll() {
-        wp_enqueue_script('coralscroll-js',  $this->plugin_url . '/resources/js.js', array('jquery'));
-        wp_enqueue_style('coralscroll-css',  $this->plugin_url . '/resources/css.css');
+        wp_enqueue_script('coralscroll-js',  plugin_dir_url( __FILE__ ) . '/resources/js.js', array('jquery'));
+        wp_enqueue_style('coralscroll-css',  plugin_dir_url( __FILE__ ) . '/resources/css.css');
     }
 
     add_action( 'wp_head', 'coralscroll');
